43963580d6741fe370b6ecd6e875fd18d2de02ab
Cesar Augusto Torres Ardila <ceatorresar@unal.edu.co>
2021-03-22 18:27:35 -0500

    my image

--- docs/sketches/image2.js
+++ docs/sketches/image2.js
@@ -1,51 +1,18 @@
+
+// Imagen
 let img;
-let height =200;
-let width = 200;
+
+// Carga archivos inicial
 function preload() {
-  // load the image
-  // is not necessary bring the image more than once
-  img = loadImage('/vc/docs/sketches/leaf.jpg');
+  // Cargar imagen
+  img = loadImage('/vc/docs/sketches/green_planet.jpg');
 }
 
+// Configuración inicial
 function setup(){
-  // Create a canvas that's at least the size of the image.
+  // Crer canvas
   createCanvas(512, 512);
-  // draw the original imaga
-  image(img, 0, 0,height,width);
-  // apply negative effect
-  effect(img,"negative",250,0)
-  // apply grayscale effect
-  effect(img,"gray",0,250)
+  //Dibujar imagen
+  image(img, 0, 0, 200, 200);
   
-}
-// get the negative colors function
-function negative(colors){
-  let c = []
-  c = colors.map((e,i)=>{
-    r = i<3 ? 255-e : e;
-    return r
-  })
-  return c 
-}
-// get the grayscale color function
-function gray(colors){
-  // alternative model to more smooth gray scale
-  let c = (0.3 * colors[0]) + (0.59 * colors[1]) + (0.11 * colors[2])
-  return c 
-}
-// apply the effect and draw the image
-function effect(img,type,x,y){
-  img.loadPixels();
-  for (let i = 0; i < img.width; i++) {
-    for (let j = 0; j < img.height; j++) {
-      colors = img.get(i,j);      
-      if(type =="negative") {
-        img.set(i, j,negative(colors));
-      }else{
-        img.set(i, j,gray(colors));
-      }
-    }
-  }
-  img.updatePixels();
-  image(img, x, y,height,width);
 }
