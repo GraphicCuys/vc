dc79cd3ab1b92cbacaf0dd6a26663d562a6df9d8
leotalorac <leotalorac@unal.edu.co>
2021-03-24 09:19:26 -0500

    fix ascii

--- docs/sketches/ascii.js
+++ docs/sketches/ascii.js
@@ -10,54 +10,68 @@ function preload() {
 function setup() {
     // Create a canvas that's at least the size of the image.
     createCanvas(1000, 1000);
-    reSize(img,100)
-    effectGray(img,"gray")
+    reSize(img, 100)
+    effectGray(img, "gray")
     image(img, 0, 0);
     console.log(img)
-    toAscii(img,100,0)
+    toAscii(img, 100, 0)
 }
 const grayRamp =
-  "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
+    "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
 const rampLength = grayRamp.length;
 const getCharacterForGrayScale = grayScale =>
-  grayRamp[Math.ceil(((rampLength - 1) * grayScale) / 255)];
+    grayRamp[Math.ceil(((rampLength - 1) * grayScale) / 255)];
 
 const littlegray = " .:-=+*#%@";
-ASCII_CHARS = ["@", "#", "$", "%", " ?", " * ", " + ", " ; ", " : ", " , ", " . "]
+ASCII_CHARS = ["@", "%", "#", "*", "+", "=", "-", " : ", " . ", " , ", " "]
 const littlerampLength = littlegray.length;
-const othergetCharacter = grayScale =>{
+const othergetCharacter = grayScale => {
     return ASCII_CHARS[Math.floor(grayScale / 26)];
 }
 
-function toAscii(img,x,y) {
-
-    
+function toAscii(img, x, y) {
     strToPrint = ""
     img.loadPixels();
-    console.log(img.width,img.height)
+    arrcols = []
+    console.log(img.width, img.height)
     for (let i = 0; i < img.height; i++) {
         for (let j = 0; j < img.width; j++) {
             // colors of the pixel
-            colors = img.get(j,i);
-            strToPrint +=othergetCharacter(colors[0])
+            colors = img.get(j, i);
+            arrcols.push(colors[0])
+            // strToPrint +=othergetCharacter(colors[0])
         }
-        strToPrint += "\n";
-        if(i==1){
-            console.log(strToPrint.length)
-        }
+        // textSize(5)
+        // // strp = ""
+        // // for(let i = 0;i<77;i++){
+        // //     strp += strToPrint.slice(100*i,(100*i)+100)
+        // //     strp+="\n";
+        // //     if(i==1)
+        // //         console.log(strp.length)
+        // // }
+        // // console.log(strp.length)
+        // text(strToPrint, x, y);
     }
-    textSize(5)
-    // strp = ""
-    // for(let i = 0;i<77;i++){
-    //     strp += strToPrint.slice(100*i,(100*i)+100)
-    //     strp+="\n";
-    //     if(i==1)
-    //         console.log(strp.length)
-    // }
-    // console.log(strp.length)
-    text(strToPrint, x, y);
+    console.log(arrcols.length)
+    drawAscii(arrcols, img.width,x,y)
 }
-function reSize(img,new_width){
+const drawAscii = (grayScales, width,x,y) => {
+    const ascii = grayScales.reduce((asciiImage, grayScale, index) => {
+        let nextChars = getCharacterForGrayScale(grayScale);
+
+        if ((index + 1) % width === 0) {
+            nextChars += "\n";
+        }
+
+        return asciiImage + nextChars;
+    }, "");
+
+    strToPrint = ascii;
+    textSize(5)
+    textFont("Courier")
+    text(strToPrint, x, y);
+};
+function reSize(img, new_width) {
     img_height = img.height
     img_width = img.width
     new_height = new_width * img_height / img_width
