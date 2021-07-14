dfcbba34c8b96bd80bb655f93b1fd253e37b3391
Mauricio Meza <mmezab@unal.edu.co>
2021-07-14 12:46:05 -0500

    small p5 change

--- docs/sketches/3dImplementation.js
+++ docs/sketches/3dImplementation.js
@@ -9,13 +9,20 @@ function preload(){
 
 
 function setup() {
-  background(190, 255, 255);
-  createCanvas(w, h, WEBGL);  
+  	background(190, 255, 255);
+  	createCanvas(w, h, WEBGL);  
 }
 
 function draw() {
-  texture(img);
-  textureMode(NORMAL);
-  sphere();
+	pointLight(255,255,255, 25, -50, 250)
+  	noStroke()
+  	//ambientLight(255)
+  	background(150, 150, 150);
+  	//ambientMaterial(255,0,0)
+  	//specularMaterial(200,200,200)
+  	texture(img);
+  	rotateY(angle)
+  	sphere(100);
+  	angle += 0.02
 
 }
